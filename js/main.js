9564db5f86738241f6eb73b5d41e6926e0f952bc
Justin McGirr <mcgirr.ju@gmail.com>
2013-11-16 14:02:06 -0800

    OOooh yeah I'm brilliant\!\!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,57 @@
 ﻿define(function (require) {
-    var ko = require("knockout");
-    var PerfChart = require("perf/PerfChart");
-    var $ = require("jquery");
+//     var ko = require("knockout");
+//     var PerfChart = require("perf/PerfChart");
+//     var $ = require("jquery");
 
+    return function main() {
+      function observable(obj) {
+        obj.__cbs = {};
+        obj.on = function (evt, cb) {
+          (obj.__cbs[evt] = obj.__cbs[evt] || []).push(cb);};
+        obj.fire = function (evt/* args ... */) {
+          var args = Array.prototype.slice.call(arguments, 1);
+          (obj.__cbs[evt] || []).forEach(function (cb) {
+            cb.apply(obj, args);})};};
+      function Vec2(x, y) {
+        observable(this);
+        Object.defineProperty(this, "x", {
+          get: function () { return this._x },
+          set: function (x) {
+            this._x = x;
+            this.fire("changed", x);}});
+        Object.defineProperty(this, "y", {
+          get: function () { return this._y },
+          set: function (y) {
+            this._y = y;
+            this.fire("changed", y);}});
+        this.x = +(x || 0);
+        this.y = +(y || 0);
+      }
+      function Entity(x, y, w, h) {
+        var self = this;
+        observable(self);
+        self.pos = new Vec2(x, y);
+        self.pos.on("changed", function(val) {
+          self.fire("moved", self.pos);});}
+      function EntityPresenter(entity) {
+        var self = this;
+        var elem = document.createElement('div');
+        elem.className = 'entity';
+        entity.on("moved", function (pos) {
+          elem.style.top = pos.y + 'px';
+          elem.style.left = pos.x + 'px';});
+        document.body.appendChild(elem);}
+      var entities = [];
+      for (var i = 0; i < 100; i++) {
+        var entity = new Entity(Math.random()*400, Math.random()*400, Math.random()*400, Math.random()*400);
+        new EntityPresenter(entity);
+        entities.push(entity);}
+      function gameLoop() {
+        for (var i = 0; i < 100; i++) {
+          entities[i].pos.y += 1;}
+        requestAnimationFrame(gameLoop);}
+      gameLoop();
+    };
     return function main() {
         function Vec2(x, y) {
             var self = this;
