9d3d7aae6b108d522bf56d420d400ebc5262b26b
Justin McGirr <mcgirr.ju@gmail.com>
2013-11-16 22:28:10 -0800

    Fix the fucking newlines.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 ﻿define(function (require) {
-    var ko = require("knockout");
-    var PerfChart = require("perf/PerfChart");
-    var $ = require("jquery");
-    var addBindings = require("customBindings");
+	var ko = require("knockout");
+	var PerfChart = require("perf/PerfChart");
+	var $ = require("jquery");
+	var addBindings = require("customBindings");
 	
 	var Game = require("Game");
 
-    return function main() {
-        var world = {
-            enemies: ko.observableArray([]),
-            friendos: ko.observableArray([]),
-            you: {},
+	return function main() {
+		var world = {
+			enemies: ko.observableArray([]),
+			friendos: ko.observableArray([]),
+			you: {},
 			gameState: "playing" //playing, gameover
-        };
+		};
 		
 		var game = new Game(world);
 		
-        addBindings(ko.bindingHandlers);
+		addBindings(ko.bindingHandlers);
 
-        ko.applyBindings(world);
+		ko.applyBindings(world);
 
-        var chart = new PerfChart();
-        $('.perfChart')[0].appendChild(chart.elm);
+		var chart = new PerfChart();
+		$('.perfChart')[0].appendChild(chart.elm);
 
-        var worldTime = new Date().getTime();
-        (function GameLoop() {
-            self.requestAnimationFrame(GameLoop);
+		var worldTime = new Date().getTime();
+		(function GameLoop() {
+			self.requestAnimationFrame(GameLoop);
 
-            var newTime = new Date().getTime();
-            var tickTime = newTime - worldTime;
-            worldTime = newTime;
+			var newTime = new Date().getTime();
+			var tickTime = newTime - worldTime;
+			worldTime = newTime;
 
-            chart.addDataPoint(tickTime);
+			chart.addDataPoint(tickTime);
 
 			function applyTick(entity) {
 				entity.tick(tickTime);
 			}
 
-            world.enemies().forEach(applyTick);
+			world.enemies().forEach(applyTick);
 			world.friendos().forEach(applyTick);
 			
 			applyTick(world.you);
 			
 			applyTick(game);
-        })();
-    }
+		})();
+	}
 });
